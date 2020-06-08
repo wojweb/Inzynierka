3e34160d80796a45a8f1533ec6e4145bda171cde
Szymon Wojtaszek <wojtaszekszymon@wp.pl>
2020-06-08 17:06:42 +0200

    dodaje dane mbst

---
+++ database/mbst/tree_generator.jl
@@ -0,0 +1,22 @@
+using MyGraph
+
+sizes = [30, 40, 50, 60] # rozmiary generowanych grafów 
+n = 5 # liczba egzemplarzy o jednakowym rozmiarze
+
+open("database/mbst/trees.txt", "w") do io
+    write(io, "$(length(sizes) * n)\n")
+    for size = sizes
+        for i = 1:n
+            write(io, "$(size)\n")
+            g = generateConnectedGraph(size)
+            for v = 1:size
+                for vi = v + 1:size
+                    write(io, "$(weight(g, v, vi)) ")
+                end
+                write(io, "\n")
+            end
+        end
+    end
+end
+
+
---
+++ database/mbst/tree_integer_solver.jl
@@ -0,0 +1,162 @@
+using MyGraph
+using JuMP
+using GLPK
+
+function mbst_opt(h::Graph, b::Int)::Int
+    g = deepcopy(h)
+    model = Model(GLPK.Optimizer)
+    intModel = Model(GLPK.Optimizer)
+    
+    subsets = Vector{Vector{Int}}(undef, 0)
+    is_feasible = false
+    @variable(model, 0 <= x[vi in vertices(g), vj in vertices(g);
+        vi > vj && has_edge(g, vi, vj)])
+    
+    @variable(intModel, 0 <= y[vi in vertices(g), vj in vertices(g);
+        vi > vj && has_edge(g, vi, vj)], Bin)
+    
+    
+    ex = AffExpr()
+    ey = AffExpr()
+    
+    for vi in vertices(g), vj in vertices(g)
+        if vi > vj && has_edge(g, vi, vj)
+            add_to_expression!(ex, weight(g, vi, vj), x[vi, vj])
+            add_to_expression!(ey, weight(g, vi, vj), y[vi, vj])
+            
+        end
+    end
+    
+    @objective(model, Min, ex)
+    @objective(intModel, Min, ey)
+    
+    ex = AffExpr()
+    ey = AffExpr()
+    
+    for vi in vertices(g), vj in vertices(g)
+        if vi > vj && has_edge(g, vi, vj)
+            add_to_expression!(ex, 1, x[vi, vj])
+            add_to_expression!(ey, 1, y[vi, vj])
+    
+        end
+    end
+    @constraint(model, ex == nv(g) - 1)
+    @constraint(intModel, ey == nv(g) - 1)
+
+    ex = AffExpr()
+    ey = AffExpr()
+
+    for vi in vertices(g)
+        ex = AffExpr()
+        ey = AffExpr()
+        for vj in vertices(g)
+            if has_edge(g, vi, vj)
+                if vi > vj
+                    add_to_expression!(ex, 1, x[vi,vj])
+                    add_to_expression!(ey, 1, y[vi,vj])
+                else
+                    add_to_expression!(ex, 1, x[vj, vi])
+                    add_to_expression!(ey, 1, y[vj, vi])
+                end
+            end
+        end
+        @constraint(model, ex <= b)
+        @constraint(intModel, ey <= b)
+    end
+
+    
+    
+    
+    while !is_feasible
+        optimize!(model)
+        if termination_status(model) != MOI.OPTIMAL
+            error(("Nie znalezniono optymalnego rozwiązania", termination_status(model), model))
+        end
+        is_feasible = true
+    
+        for vx in vertices(g), vy in vertices(g)
+            if vx > vy
+                gprim = Graph(nv(h) + 2, true) # source = nv, sink = nv + 1
+                source = nv(h) + 1
+                sink = nv(h) + 2
+                for vi in vertices(g), vj in vertices(g)
+                    if vi > vj && has_edge(g, vi, vj)
+                        add_edge!(gprim, vi, vj, 0.5 * round(value(x[vi, vj]), digits=8))
+                        add_edge!(gprim, vj, vi, 0.5 * round(value(x[vi, vj]), digits=8))
+                    end
+                end
+    
+                for vi in vertices(g)
+                    sum = 0.
+                    for vj in delta(g, vi)
+                        sum += value(x[max(vi, vj),min(vi, vj)])
+                    end
+                    add_edge!(gprim, source, vi, sum / 2)
+                    add_edge!(gprim, vi, sink, 1.)
+                end
+                add_edge!(gprim, source, vx, typemax(Float64))
+                add_edge!(gprim, source, vy, typemax(Float64))
+    
+                flow, visited = fordfulkerson(gprim, source, sink)
+                if round(flow, digits = 4)< nv(g)
+                    filter!(x -> x != source, visited)
+                    is_feasible = false
+                    ex = AffExpr()
+                    ey = AffExpr()
+    
+                    for vi in visited, vj in visited
+                        if vi > vj && has_edge(g, vi, vj)
+                            add_to_expression!(ex, 1, x[vi, vj])
+                            add_to_expression!(ey, 1, y[vi, vj])
+    
+                        end
+                    end
+                    @constraint(model, ex <= length(visited) - 1)
+                    @constraint(intModel, ey <= length(visited) - 1)
+    
+                    break
+                end
+    
+            end
+        end
+    end    
+    optimize!(intModel)
+    
+    return  round(objective_value(intModel))
+    
+end
+
+bounds = [2,3,5]
+
+bounds = [5]
+
+for b = bounds
+    open("database/mbst/optsb$(b)p.txt", "w") do io
+
+
+        content = Base.read("database/mbst/treesp.txt",String)
+        content_float = [parse(Float64,x) for x in split(content)]
+
+        n = Int(content_float[1])
+        content_float = content_float[2:end]
+
+        for i = 1:n
+            content_float
+
+            size = Int(content_float[1])
+            content_float = content_float[2:end]
+
+            g = Graph(size)
+            for v = 1:size
+                for vi = v + 1:size
+                    add_edge!(g, v, vi, content_float[1])
+                    content_float = content_float[2:end]
+                end
+            end
+            println("zaczynam")
+            opt = mbst_opt(g, b)
+            write(io, "$(opt)\n")
+
+        end
+    end
+end
